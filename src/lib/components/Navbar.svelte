66324ec3f136c946f040ef0ddfd50a47b75c6341
Federico Lo Dolce <flodolce92@gmail.com>
2026-02-24 03:22:47 +0100

    feat(navbar): implemented simple navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,26 @@
+<script>
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { user } = $props();
+</script>
+
+<header>
+	<nav class="flex items-center justify-between px-6 py-3">
+		<a href="/" class="text-xl font-bold">
+			<img src={favicon} alt="Logo" class="h-6 w-6 inline-block mr-2" />
+		</a>
+
+		<Button variant="outline" class="w-64">Ctrl K</Button>
+
+		<a href="/profile">
+			<Avatar.Root>
+				<Avatar.Image src={user?.image} alt={user?.name} />
+				<Avatar.Fallback>
+					{user?.name ? user.name[0] : 'U'}
+				</Avatar.Fallback>
+			</Avatar.Root>
+		</a>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/Navbar.svelte';
 
-	let { children } = $props();
-	import "../app.css";
+	let { data, children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<Navbar user={data.session?.user} />
+<main class="max-w-5xl mx-auto px-6 py-8">
+	{@render children()}
+</main>
